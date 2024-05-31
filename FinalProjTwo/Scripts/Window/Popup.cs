ec488230d2e626ff33ca5e7ffdaaf96b2f8b1c3b
theo lindqvist <theo.lindqvist@elev.ga.ntig.se>
2024-05-31 13:40:33 +0200

    Updated start-popup version

--- FinalProjTwo/Scripts/Window/Popup.cs
+++ FinalProjTwo/Scripts/Window/Popup.cs
@@ -37,8 +37,8 @@ public sealed class StartPopup : PopupWindow
     public override void Draw()
     {
         base.Draw();
-        TextHandling.DrawScreenCenteredText(["Paint.TO v2.106"], (int)windowRect.Y + 30, 80, 0, Color.Black);
-        TextHandling.DrawScreenCenteredText(["Patch notes:", "-(2.1)Rectangle selection changes are now visible in real time", "-Small fixes"],
+        TextHandling.DrawScreenCenteredText(["Paint.TO v2.116"], (int)windowRect.Y + 30, 80, 0, Color.Black);
+        TextHandling.DrawScreenCenteredText(["Patch notes:", "-(2.1)Rectangle selection changes are now visible in real time", "-Made program useable on other resolutions"],
                                             (int)windowRect.Y + 175, 20, 30, Color.Black);
     }
 }
